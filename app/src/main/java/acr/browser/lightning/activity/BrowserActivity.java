1ae54a4d96162d3bd0a443fd9e63e79086aedc19
Gal Levinsky <gal.levinsky@gmail.com>
2018-02-26 10:38:22 +0200

    v. 136

--- app/src/main/java/acr/browser/lightning/activity/BrowserActivity.java
+++ app/src/main/java/acr/browser/lightning/activity/BrowserActivity.java
@@ -97,6 +97,7 @@ import java.util.List;
 
 import javax.inject.Inject;
 
+import acr.browser.lightning.BuildConfig;
 import acr.browser.lightning.R;
 import acr.browser.lightning.app.BrowserApp;
 import acr.browser.lightning.browser.BookmarksView;
@@ -274,6 +275,12 @@ public abstract class BrowserActivity extends ThemableBrowserActivity implements
         initialize(savedInstanceState);
 
         showSearchNotification();
+
+        if (BuildConfig.IS_INCOGNITO_MODE_DEFAULT){
+            Intent intent = new Intent(BrowserActivity.this, IncognitoActivity.class);
+//            intent.setData(Uri.parse(event.url));
+            startActivity(intent);
+        }
 
     }
 
